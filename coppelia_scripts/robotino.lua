b3857f5fbb8e7e47ad1ba4a0a1839ebaf084cbde
Leonardo Hemerly <leohmcs@gmail.com>
2021-09-17 10:35:37 -0300

    First commit.

---
+++ coppelia_scripts/robotino.lua
@@ -0,0 +1,168 @@
+function publish_gt_odometry()
+    motor0Vel = sim.getJointTargetVelocity(motor0Handle, -1)
+    motor1Vel = sim.getJointTargetVelocity(motor1Handle, -1)
+    motor2Vel = sim.getJointTargetVelocity(motor2Handle, -1)
+    
+    wheelVelVec = {motor0Vel, motor1Vel, motor2Vel}
+    
+    robotVel = matrix.mul(wheelVelVec, Mdir)
+    sim.addLog(sim.verbosity_scriptinfos, robotVel)
+    
+    p = sim.getObjectPosition(robotHandle, -1)
+    o = sim.getObjectQuaternion(robotHandle, -1)
+    
+    odom = {
+        header= {
+            stamp= simROS.getTime(),
+            frame_id= 'odom'
+        },
+        child_frame_id= 'base_link',
+        pose= {
+            pose= {
+                position= {
+                    x=p[1],
+                    y=p[2],
+                    z=p[3]
+                },
+                orientation= {
+                    x=o[1],
+                    y=o[2],
+                    z=o[3],
+                    w=o[4]
+                }
+            }
+        },
+        twist= {
+            twist= {
+                linear= {
+                    x=robotVel[1],
+                    y=robotVel[2],
+                    z=0
+                },
+                angular= {
+                    x=0,
+                    y=0,
+                    z=robotVel[3]
+                }
+            }
+        }
+    }
+    simROS.publish(publisher_odom, odom)
+    
+    simROS.sendTransform({
+        header={
+           stamp = simROS.getTime(), 
+            frame_id = 'odom'}, 
+            child_frame_id = 'base_link', 
+            transform = {
+                translation = {x=p[1], y=p[2], z=0}, 
+                rotation = {x = quat_rot[1], y = quat_rot[2], z = quat_rot[3], w = quat_rot[4]}
+            }
+        }
+    )
+    
+    last_time = current_time
+end
+
+
+function publish_joint_state(jointHandle, jointName, publisher)
+    jointPos = sim.getJointPosition(jointHandle)
+    jointVel = sim.getJointTargetVelocity(jointHandle, -1)
+    jointEffort = sim.getJointForce(jointHandle)
+    
+    msg = {
+        header = {
+            stamp = simROS.getTime(),
+            frame_id = jointName
+        },
+        name = {jointName},
+        position = {jointPos},
+        velocity = {jointVel},
+        effort = {jointEffort}
+    }
+    
+    simROS.publish(publisher, msg)
+end
+
+
+function publish_gt_pose()
+    p = sim.getObjectPosition(robotHandle, -1)
+    o = sim.getObjectQuaternion(robotHandle, -1)
+    
+    pose_stamped = {
+        header = {
+            stamp= simROS.getTime(),
+            frame_id= 'odom'
+        },
+        pose = {
+            position = {x = p[1], y = p[2], z = p[3]},
+            orientation = {x = o[1], y = o[2], z = o[3], w = o[4]}
+        }
+    }
+    
+    simROS.publish(publisher_gt_pose, pose_stamped)
+end
+
+
+function velocity_callback(msg)
+    dx = msg.linear.x
+    dy = msg.linear.y
+    dt = msg.angular.z
+    
+    move(dx, dy, dt)
+end
+
+
+function move(dx, dy, dt)
+    q = Vector3{dx, dy, dt}
+    v = Minv * q
+    
+    sim.setJointTargetVelocity(motor0Handle, v[1])
+    sim.setJointTargetVelocity(motor1Handle, v[2])
+    sim.setJointTargetVelocity(motor2Handle, v[3])
+end
+
+
+function sysCall_init()
+    robotHandle = sim.getObjectHandle(sim.handle_self)
+    robotName = sim.getObjectName(robotHandle)
+    robotPrefix = robotName:gsub("#","")  -- Remove #
+    
+    motor0Handle = sim.getObjectHandle("wheel0_joint")
+    motor1Handle = sim.getObjectHandle("wheel1_joint")
+    motor2Handle = sim.getObjectHandle("wheel2_joint")
+
+    -- Kinematic model
+    L = 0.135   -- Meters
+    r = 0.040   -- Meters
+    require 'matrix'
+    Minv = Matrix(3, 3, {-math.sqrt(3)/2, 0.5, L, 0, -1, L, math.sqrt(3)/2, 0.5, L})
+    Minv = Minv * (1/r)
+    
+    if simROS then
+        sim.addLog(sim.verbosity_scriptinfos, "ROS interface was found.")
+        
+        -- Prepare GT pose
+        publisher_gt_pose = simROS.advertise("/" .. robotPrefix .. '/gt_pose', 'geometry_msgs/PoseStamped')
+        
+        publisher_joint0_state = simROS.advertise("/" .. robotPrefix .. "/joint0_state", "sensor_msgs/JointState")
+        publisher_joint1_state = simROS.advertise("/" .. robotPrefix .. "/joint1_state", "sensor_msgs/JointState")
+        publisher_joint2_state = simROS.advertise("/" .. robotPrefix .. "/joint2_state", "sensor_msgs/JointState")
+        
+        subscriber_velocity = simROS.subscribe("/" .. robotPrefix .. "/cmd_vel", "geometry_msgs/Twist", "velocity_callback")
+    else
+        sim.addLog(sim.verbosity_scripterrors, "ROS interface was not found. Cannot run.")    
+    end
+end
+
+
+function sysCall_actuation()
+    publish_gt_pose()
+    
+    joints_handlers = {motor0Handle, motor1Handle, motor2Handle}
+    joints_frames = {"wheel0_joint", "wheel1_joint", "wheel2_joint"}
+    joints_publishers = {publisher_joint0_state, publisher_joint1_state, publisher_joint2_state}
+    for i=1,3,1 do  
+        publish_joint_state(joints_handlers[i], joints_frames[i], joints_publishers[i])
+    end
+end
---
+++ scripts/odometry.py
@@ -0,0 +1,111 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import rospy
+from geometry_msgs.msg import PoseStamped, Vector3, Twist
+from nav_msgs.msg import Odometry
+from sensor_msgs.msg import JointState
+
+import tf
+
+import numpy as np
+
+
+class Odom:
+    def __init__(self):
+        L = 0.135   # Meters
+        r = 0.040   # Meters
+        # Kinematic model
+        self.Mdir = np.array([[-r/np.sqrt(3), 0, r/np.sqrt(3)], [r/3, -2*r/3, r/3], [r/(3*L), r/(3*L), r/(3*L)]])
+
+        self.pose_covariance = [0, 0, 0, 0, 0, 0,
+                                0, 0, 0, 0, 0, 0,
+                                0, 0, 0, 0, 0, 0,
+                                0, 0, 0, 0, 0, 0,
+                                0, 0, 0, 0, 0, 0,
+                                0, 0, 0, 0, 0, 0]
+
+        self.twist_covariance = [0, 0, 0, 0, 0, 0,
+                                0, 0, 0, 0, 0, 0,
+                                0, 0, 0, 0, 0, 0,
+                                0, 0, 0, 0, 0, 0,
+                                0, 0, 0, 0, 0, 0,
+                                0, 0, 0, 0, 0, 0]
+
+        self.odom_broadcaster = tf.broadcaster.TransformBroadcaster()
+
+        self.pose_stamped = PoseStamped()
+        self.joint0_state = JointState()
+        self.joint1_state = JointState()
+        self.joint2_state = JointState()
+
+        pose_stamped_sub = rospy.Subscriber('gt_pose', PoseStamped, self.pose_stamped_callback)
+        joint0_state_sub = rospy.Subscriber('joint0_state', JointState, self.joint0_state_callback)
+        joint1_state_sub = rospy.Subscriber('joint1_state', JointState, self.joint1_state_callback)
+        joint2_state_sub = rospy.Subscriber('joint2_state', JointState, self.joint2_state_callback)
+
+        self.odom_pub = rospy.Publisher('odom', Odometry, queue_size=100)
+
+    def pose_stamped_callback(self, msg):
+        self.pose_stamped = msg
+
+    def joint0_state_callback(self, msg):
+        self.joint0_state = msg
+
+    def joint1_state_callback(self, msg):
+        self.joint1_state = msg
+
+    def joint2_state_callback(self, msg):
+        self.joint2_state = msg
+
+    def get_velocity(self):
+        w0 = self.joint0_state.velocity[0]
+        w1 = self.joint1_state.velocity[0]
+        w2 = self.joint2_state.velocity[0]
+
+        w_vec = np.array([w0, w1, w2])
+
+        q_vel = np.matmul(self.Mdir, w_vec)
+
+        return q_vel
+
+    def publish_odom(self):
+        pose = self.pose_stamped.pose
+        vel = self.get_velocity()
+
+        ## Cartographer is configured to provide odom frame ##
+        # pos = np.array([pose.position.x, pose.position.y, 0.])
+        # quat = np.array([pose.orientation.x, pose.orientation.y, pose.orientation.z, pose.orientation.w])
+        # self.odom_broadcaster.sendTransform (
+        #     pos,
+        #     quat,
+        #     rospy.Time.now(),
+        #     "base_link",
+        #     "odom"
+        # )
+
+        # fill message
+        msg = Odometry()
+        msg.header.stamp = rospy.Time.now()
+        msg.header.frame_id = 'odom'
+
+        msg.child_frame_id = 'base_link'
+
+        msg.pose.pose = pose
+        msg.pose.covariance = self.pose_covariance
+
+        msg.twist.twist = Twist(Vector3(vel[0], vel[1], 0.), Vector3(0., 0., vel[2]))
+        msg.twist.covariance = self.twist_covariance
+
+        self.odom_pub.publish(msg)
+
+
+node = Odom()
+node_name = 'odom'
+rospy.init_node(node_name)
+rospy.loginfo(node_name + " inicializado com sucesso.")
+
+rate = rospy.Rate(10)
+while not rospy.is_shutdown():
+     node.publish_odom()
+     rate.sleep()
