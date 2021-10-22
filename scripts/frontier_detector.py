4e7425167cbf2162cd0022b71cc2ff847d3b9277
leohmcs <leohmcs@gmail.com>
2021-10-22 19:18:02 -0300

    Preprocess map image to remove false free pixels.

--- scripts/frontier_detector.py
+++ scripts/frontier_detector.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 #coding: utf-8
 
+from typing import final
 import rospy
 from geometry_msgs.msg import Point, PointStamped
 from nav_msgs.msg import OccupancyGrid, Odometry
@@ -18,18 +19,24 @@ class FrontierDetector:
         self.map = None
         self.map_origin = None
         self.map_resolution = None
-        self.map_img = None
 
         self.frontiers = None
 
         self.robot_position = None
         self.robot_ori = None
 
-        map_sub = rospy.Subscriber('map', OccupancyGrid, callback=self.map_callback)
         odom_sub = rospy.Subscriber('odom', Odometry, callback=self.odom_callback)
+        map_sub = rospy.Subscriber('/map', OccupancyGrid, callback=self.map_callback)
 
         self.frontiers_pub = rospy.Publisher('map/frontiers', PointArray, queue_size=100)
         self.nearest_frontier_pub = rospy.Publisher('map/frontiers/nearest', PointStamped, queue_size=100)
+
+    def odom_callback(self, msg):
+        position = msg.pose.pose.position
+        self.robot_position = np.array([position.x, position.y])
+        quat = [msg.pose.pose.orientation.x, msg.pose.pose.orientation.y, msg.pose.pose.orientation.z, msg.pose.pose.orientation.w]
+        euler = tf_conversions.transformations.euler_from_quaternion(quat)
+        self.robot_ori = euler[2]
 
     def map_callback(self, msg):
         rr = msg.info.height
@@ -38,12 +45,24 @@ class FrontierDetector:
         self.map_origin = np.array([msg.info.origin.position.x, msg.info.origin.position.y])
         self.map_resolution = msg.info.resolution
 
-    def odom_callback(self, msg):
-        position = msg.pose.pose.position
-        self.robot_position = np.array([position.x, position.y])
-        quat = [msg.pose.pose.orientation.x, msg.pose.pose.orientation.y, msg.pose.pose.orientation.z, msg.pose.pose.orientation.w]
-        euler = tf_conversions.transformations.euler_from_quaternion(quat)
-        self.robot_ori = euler[2]
+    def map_img(self):
+        img = np.zeros(self.map.shape, dtype=np.uint8)
+        img = (1 - (self.map/100)) * 255
+        unknown = np.where(self.map == -1)
+        img[unknown] = 155
+        return img
+
+    def binary_map_img(self, occ_thresh=50, unknown_flag=-1):
+        bin_map = np.ones(self.map.shape, dtype=np.uint8) * 255
+        bin_map[self.map > occ_thresh] = 0
+        bin_map[self.map == unknown_flag] = 0
+        return bin_map
+
+    def preprocess_map_img(self, map_img, kernel=np.ones((3, 3))):
+        final_img = (255 - map_img).astype('uint8')
+        final_img = cv2.morphologyEx(final_img, cv2.MORPH_CLOSE, kernel)
+        cv2.waitKey(10)
+        return final_img
     
     def array2point_msg(self, array, is_3d=False):
         try:
@@ -75,35 +94,19 @@ class FrontierDetector:
 
             return msg
 
-    def update_map_img(self):
-        img = np.zeros(self.map.shape)
-        img = (1 - (self.map/100)) * 255
-        unknown = np.where(self.map == -1)
-        img[unknown] = 155
-        self.map_img = img.astype('uint8')
-
     def cell2position(self, cell):
-        print(cell.shape)
         map_resolution = self.map_resolution
         pos = []
         try:
             for c in cell:
-                c = c[::-1]
-                pos.append(c * map_resolution)
+                c = c[::-1] # [row. col] -> [x, y]
+                pos.append((c * map_resolution)) 
         
         except TypeError:
             cell = cell[::-1]
-            pos = cell * map_resolution
+            pos = (cell * map_resolution)
 
-        print(np.array(pos).shape)
         return np.array(pos)
-
-    def rz(self, point):
-        th = self.robot_ori
-        mat = np.array([[np.cos(th), np.sin(th), 0], [-np.sin(th), np.cos(th), 0], [0, 0, 1]])
-        point = np.append(point, 0)
-        result = np.dot(mat, point)
-        return result
 
     def get_neighbours(self, ind, map_shape, n=1):
         rr, cc = ind
@@ -127,14 +130,10 @@ class FrontierDetector:
         return neighbours
 
     def detect_frontiers(self):
-        self.update_map_img()
-        candidates = self.get_frontier_candidates(self.map_img)
-        self.frontiers = self.remove_obstacles_edges(candidates, occ_thresh=55)
-        # frontiers_points = self.remove_obstacles_edges(candidates, occ_thresh=55)
-        # self.frontiers = frontiers_points
-        # self.frontiers = self.process_result(frontiers_points)
-        # frontiers_lines = self.get_frontiers_lines(frontiers_points, threshold=40)
-        # self.frontiers = self.draw_frontiers_lines(frontiers_lines)
+        map_img = self.binary_map_img()
+        map_img = self.preprocess_map_img(map_img)
+        candidates = self.get_frontier_candidates(map_img)
+        self.frontiers = self.remove_obstacles_edges(candidates)
 
     def process_result(self, frontier_points):
         img = (frontier_points * 255).astype('uint8')
@@ -142,30 +141,23 @@ class FrontierDetector:
         img = cv2.dilate(img, np.ones((3, 3)))
         return img / 255
 
-    def get_frontier_candidates(self, m_img):
-        img = np.copy(m_img)
-        edges = cv2.Canny(img, 0, 170)
-        # cv2.imshow('edges', edges)
-        # cv2.imshow('edges c', 255 - edges)
-        # candidates = np.where(edges != 0)
+    def get_frontier_candidates(self, map_img):
+        edges = cv2.Canny(map_img, 0, 170)
         return edges
 
-    def remove_obstacles_edges(self, candidates, unknown_flag=-1, occ_thresh=49):
+    def remove_obstacles_edges(self, candidates, unknown_flag=-1, occ_thresh=65):
         '''
         Input: all edges returned by Canny algorithm
         Output: real frontier pixels
         '''
         m = self.map
         frontier_points = np.zeros(m.shape)
-        it = np.nditer(candidates, flags=['multi_index'])
-        for i in it:
-            if m[it.multi_index] == unknown_flag:
-                continue
-
-            neighbours = self.get_neighbours(it.multi_index, m.shape)
+        cand_ind = np.transpose(np.where(candidates != 0))
+        for c in cand_ind:
+            neighbours = self.get_neighbours((c[0], c[1]), m.shape, n=3)
             cand_region = self.map[neighbours[0], neighbours[1]]
             if np.all(cand_region < occ_thresh) and np.any(cand_region == unknown_flag):
-                    frontier_points[it.multi_index] = 1
+                    frontier_points[c[0], c[1]] = 1
         
         return frontier_points
 
@@ -201,7 +193,7 @@ class FrontierDetector:
             return None
 
         nearest = np.argmin(np.linalg.norm(frontiers_pos - robot_position, axis=1))
-        return self.rz(frontiers_pos[nearest]), frontiers_ind[nearest] 
+        return frontiers_pos[nearest], frontiers_ind[nearest] 
     
     # TODO
     def biggest_frontier(self):
@@ -216,7 +208,7 @@ def parse_args():
 
 
 # args = parse_args()
-fig = plt.figure(figsize=(10, 5))
+fig = plt.figure(figsize=(20, 10))
 fig.suptitle(rospy.get_namespace().replace('/', ''))
 map_axes = fig.add_subplot(121)
 map_axes.set_title('Map')
@@ -245,9 +237,6 @@ while not rospy.is_shutdown():
         rospy.sleep(1)
         continue
 
-    # frontiers_msg = detector.array2point_msg(frontiers)
-    # detector.publish_frontiers(PointArray(frontiers_msg))
-
     nearest, ind = detector.nearest_frontier()
     if nearest is None:
         continue
@@ -258,9 +247,8 @@ while not rospy.is_shutdown():
     msg.point.x = nearest[0]
     msg.point.y = nearest[1]
     detector.nearest_frontier_pub.publish(msg)
-    rospy.loginfo('{} updated nearest frontier.'.format(rospy.get_namespace()))
 
-    map_axes.imshow(detector.map_img, cmap='gray', origin='lower')
+    map_axes.imshow(detector.map_img(), cmap='gray', origin='lower')
 
     frontiers_img = (frontiers * 255).astype('uint8')
     
