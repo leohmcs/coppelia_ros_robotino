96ae58911e8650080b91df0b721f1614a97b8d82
leohmcs <leohmcs@gmail.com>
2021-10-27 10:37:35 -0300

    Clean frontier detector code.

--- scripts/frontier_detector.py
+++ scripts/frontier_detector.py
@@ -64,7 +64,6 @@ class FrontierDetector:
     def preprocess_map_img(self, map_img, kernel=np.ones((3, 3))):
         final_img = (255 - map_img).astype('uint8')
         final_img = cv2.morphologyEx(final_img, cv2.MORPH_CLOSE, kernel)
-        cv2.waitKey(10)
         return final_img
     
     def array2point_msg(self, array, is_3d=False):
@@ -138,12 +137,6 @@ class FrontierDetector:
         candidates = self.get_frontier_candidates(map_img)
         self.frontiers = self.remove_obstacles_edges(candidates)
 
-    def process_result(self, frontier_points):
-        img = (frontier_points * 255).astype('uint8')
-        img = cv2.erode(img, np.ones((3, 3)))
-        img = cv2.dilate(img, np.ones((3, 3)))
-        return img / 255
-
     def get_frontier_candidates(self, map_img):
         edges = cv2.Canny(map_img, 0, 170)
         return edges
@@ -163,24 +156,6 @@ class FrontierDetector:
                     frontier_points[c[0], c[1]] = 1
         
         return frontier_points
-
-    def get_frontiers_lines(self, frontier_points, use_probabilistc=True, rho=1, th=np.pi/180, threshold=80, min_line_length=30, max_line_gap=20):
-        '''
-        Apply Hough Lines algorithm to get frontiers as lines instead os points
-        '''
-        img = (frontier_points * 255).astype('uint8')
-        frontiers = cv2.HoughLinesP(img, rho, th, threshold, minLineLength=min_line_length, maxLineGap=max_line_gap)
-
-        return frontiers
-
-    def draw_frontiers_lines(self, lines=None):
-        img = np.zeros(self.map.shape)
-        if lines is not None:
-            for i in range(0, len(lines)):
-                l = lines[i][0]
-                cv2.line(img, (l[0], l[1]), (l[2], l[3]), 255, 3, cv2.LINE_AA)
-
-        return img
 
     def num_frontiers(self):
         return len(self.frontiers)
